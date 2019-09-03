16a6727d7f63ce9f49d4b6b4229ea45a7a944fed
Nami <htram@mail.usf.edu>
2019-09-03 12:46:50 -0400

    Version 3 of HelloWorld project

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ namespace HelloWorld
             // Pause to display Hello World!
             Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Nami.");
+            Console.ReadKey(true);
+            Console.WriteLine("Nami, like Tsunami, but not Tsunami.");
+            Console.ReadKey(true);
+            Console.WriteLine("Have you smiled?");
+            Console.ReadKey(true);
+            Console.WriteLine("That's good! Keep being positive!");
+            Console.ReadKey(true);
+            Console.WriteLine("Have a nice day!");
             Console.ReadKey(true);
         }
     }
