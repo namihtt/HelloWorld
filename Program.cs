575712e4646b589a38d20214132cbdc999f3af27
Nami <htram@mail.usf.edu>
2019-09-03 12:53:25 -0400

    Version 4 of HelloWorld project with added comments

--- Program.cs
+++ Program.cs
@@ -6,19 +6,30 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // Pause to display Hello World!
+            // Display Hello World! My name is Nami
             Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World!");
             Console.WriteLine("My name is Nami.");
+
+            // Pause to wait for user to read the displayed words and press any key to display the next line
             Console.ReadKey(true);
             Console.WriteLine("Nami, like Tsunami, but not Tsunami.");
+
+            // Pause to wait for user to read the displayed words and press any key to display the next line
             Console.ReadKey(true);
             Console.WriteLine("Have you smiled?");
+
+            // Pause to wait for user to read the displayed words and press any key to display the next line
             Console.ReadKey(true);
             Console.WriteLine("That's good! Keep being positive!");
+
+            // Pause to wait for user to read the displayed words and press any key to display the next line
             Console.ReadKey(true);
             Console.WriteLine("Have a nice day!");
+
+            // Pause to wait for user to read the displayed words and press any key to close
             Console.ReadKey(true);
+
         }
     }
 }
